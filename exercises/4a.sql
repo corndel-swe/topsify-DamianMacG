5e8cb38e08f7553df72d09c35a975d0e73432dcf
Damian <damiangildea12@gmail.com>
2024-10-07 11:30:54 +0100

    4a and 4b tests passing

--- exercises/4a.sql
+++ exercises/4a.sql
@@ -1,3 +1,8 @@
 -- List the name of every album in the database
 -- Include the artist name
 -- (Make sure to select the album name first, then the artist name)
+
+SELECT albums.name AS album_name, artists.name AS artist_name
+FROM albums
+JOIN artists ON albums.artist_id = artists.id;
+
